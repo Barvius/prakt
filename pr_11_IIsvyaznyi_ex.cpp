fc19c36ebc746a77631dc76fc2c92cf0cf61da22
barvius <barabanshikov.oleg@yandex.ru>
2017-07-06 21:13:03 +0300

    пример двусвязного списка

--- pr_11_IIsvyaznyi_ex.cpp
+++ pr_11_IIsvyaznyi_ex.cpp
@@ -4,34 +4,32 @@
 #include "stdafx.h"
 #include <stdlib.h>
 
-typedef struct Car{
-	char mark[20] = { NULL };
-	char number[10] = { NULL };
-	char surname[20] = { NULL };
-} Car;
+typedef struct Order {
+	//struct Check_acc payer;
+	//struct Check_acc recivier;
+	float sum;
+}Order;
 
 typedef struct _Node {
-	Car value;
+	Order val;
 	struct _Node *next;
 	struct _Node *prev;
 } Node;
 
-
-typedef struct _DblLinkedList {
+typedef struct _List {
 	size_t size;
 	Node *head;
 	Node *tail;
-} DblLinkedList;
+} List;
 
-
-DblLinkedList* createDblLinkedList() {
-	DblLinkedList *tmp = new DblLinkedList;
+List* createLinkedList() { // создает список
+	List *tmp = new List;
 	tmp->size = 0;
 	tmp->head = tmp->tail = NULL;
 	return tmp;
 }
 
-void deleteDblLinkedList(DblLinkedList **list) {
+void deleteLinkedList(List **list) { // удаляет список
 	Node *tmp = (*list)->head;
 	Node *next = NULL;
 	while (tmp) {
@@ -43,85 +41,34 @@ void deleteDblLinkedList(DblLinkedList **list) {
 	(*list) = NULL;
 }
 
-void pushFront(DblLinkedList *list, Car data) {
+void push(List *list, Order data) { // добавляет/создает элемент
 	Node *tmp = new Node;
-	if (tmp == NULL) {
-		exit(1);
-	}
-	tmp->value = data;
-	tmp->next = list->head;
-	tmp->prev = NULL;
-	if (list->head) {
-		list->head->prev = tmp;
-	}
-	list->head = tmp;
-	if (list->tail == NULL) {
-		list->tail = tmp;
-	}
-	list->size++;
-}
-
-Car popFront(DblLinkedList *list) {
-	Node *prev;
-	Car tmp;
-	if (list->head == NULL) {
-		exit(2);
-	}
-
-	prev = list->head;
-	list->head = list->head->next;
-	if (list->head) {
-		list->head->prev = NULL;
-	}
-	if (prev == list->tail) {
-		list->tail = NULL;
-	}
-	tmp = prev->value;
-	free(prev);
-
-	list->size--;
-	return tmp;
-}
-
-void pushBack(DblLinkedList *list, Car value) {
-	Node *tmp = new Node;
-	if (tmp == NULL) {
-		exit(3);
-	}
-	tmp->value = value;
-	tmp->next = NULL;
-	tmp->prev = list->tail;
-	if (list->tail) {
-		list->tail->next = tmp;
-	}
-	list->tail = tmp;
-	if (list->head == NULL) {
+	tmp->val = data;
+	if (!list->size) { // если список пустой то создаем первый элемент
+		tmp->next = list->head;
+		tmp->prev = NULL;
+		if (list->head) {
+			list->head->prev = tmp;
+		}
 		list->head = tmp;
+		if (list->tail == NULL) {
+			list->tail = tmp;
+		}
+	} else { // если не пустой, то докидываем в конец записи
+		tmp->next = NULL;
+		tmp->prev = list->tail;
+		if (list->tail) {
+			list->tail->next = tmp;
+		}
+		list->tail = tmp;
+		if (list->head == NULL) {
+			list->head = tmp;
+		}
 	}
 	list->size++;
 }
 
-Car popBack(DblLinkedList *list) {
-	Node *next;
-	Car tmp;
-	if (list->tail == NULL) {
-		exit(4);
-	}
-	next = list->tail;
-	list->tail = list->tail->prev;
-	if (list->tail) {
-		list->tail->next = NULL;
-	}
-	if (next == list->head) {
-		list->head = NULL;
-	}
-	tmp = next->value;
-	free(next);
-	list->size--;
-	return tmp;
-}
-
-Node* getNth(DblLinkedList *list, size_t index) {
+Node* getNth(List *list, size_t index) { // получает значение элемента по индексу, нужно для удаления
 	Node *tmp = list->head;
 	size_t i = 0;
 	while (tmp && i < index) {
@@ -131,86 +78,9 @@ Node* getNth(DblLinkedList *list, size_t index) {
 	return tmp;
 }
 
-Node* getNthq(DblLinkedList *list, size_t index) {
-	Node *tmp = NULL;
-	size_t i;
-	if (index < list->size / 2) {
-		i = 0;
-		tmp = list->head;
-		while (tmp && i < index) {
-			tmp = tmp->next;
-			i++;
-		}
-	}
-	else {
-		i = list->size - 1;
-		tmp = list->tail;
-		while (tmp && i > index) {
-			tmp = tmp->prev;
-			i--;
-		}
-	}
-	return tmp;
-}
-
-
-void printDblLinkedList(DblLinkedList *list) {
-	Node *tmp = list->head;
-	int i = 0;
-	while (tmp) {
-		printf("  %d) %-15s %-15s %-15s \n", i + 1, tmp->value.mark, tmp->value.number, tmp->value.surname);
-		i++;
-		tmp = tmp->next;
-	}
-	printf("\n");
-}
-/*
-DblLinkedList* fromArray(void *arr, size_t n, size_t size) {
-	DblLinkedList *tmp = NULL;
-	size_t i = 0;
-	if (arr == NULL) {
-		exit(7);
-	}
-
-	tmp = createDblLinkedList();
-	while (i < n) {
-		pushBack(tmp, ((char*)arr + i*size));
-		i++;
-	}
-
-	return tmp;
-}
-*/
-
-void insert(DblLinkedList *list, size_t index, Car value) {
+Order deleteNth(List *list, size_t index) { //удаляет элемент по индексу
 	Node *elm = NULL;
-	Node *ins = NULL;
-	elm = getNth(list, index);
-	if (elm == NULL) {
-		exit(5);
-	}
-	ins = (Node*)malloc(sizeof(Node));
-	ins->value = value;
-	ins->prev = elm;
-	ins->next = elm->next;
-	if (elm->next) {
-		elm->next->prev = ins;
-	}
-	elm->next = ins;
-
-	if (!elm->prev) {
-		list->head = elm;
-	}
-	if (!elm->next) {
-		list->tail = elm;
-	}
-
-	list->size++;
-}
-
-Car deleteNth(DblLinkedList *list, size_t index) {
-	Node *elm = NULL;
-	Car tmp;
+	Order tmp;
 	elm = getNth(list, index);
 	if (elm == NULL) {
 		exit(5);
@@ -221,59 +91,24 @@ Car deleteNth(DblLinkedList *list, size_t index) {
 	if (elm->next) {
 		elm->next->prev = elm->prev;
 	}
-	tmp = elm->value;
-
+	tmp = elm->val;
 	if (!elm->prev) {
 		list->head = elm->next;
 	}
 	if (!elm->next) {
 		list->tail = elm->prev;
 	}
-
 	free(elm);
-
 	list->size--;
-
 	return tmp;
 }
 
-void main(){
-	DblLinkedList *list = createDblLinkedList();
-	Car car, car2, car3;
-	sprintf(car.mark,"bmw");
-	sprintf(car.number, "123");
-	sprintf(car.surname, "bgbv");
-	sprintf(car2.mark, "audi");
-	sprintf(car2.number, "123");
-	sprintf(car2.surname, "bcxv");
-	sprintf(car3.mark, "xuy");
-	sprintf(car3.number, "456");
-	sprintf(car3.surname, "abc");
-	pushFront(list, car);
-	pushBack(list, car2);
-	insert(list, 1, car3);
-	printDblLinkedList(list);
-	deleteNth(list, 0);
-	printDblLinkedList(list);
-	/*
-	printf("length %d\n", list->size);
-	printf("nth 2 %d\n", *((int*)(getNthq(list, 2))->value));
-	printf("nth 5 %d\n", *((int*)(getNthq(list, 5))->value));
-	printf("popFront %d\n", *((int*)popFront(list)));
-	printf("popFront %d\n", *((int*)popFront(list)));
-	printf("head %d\n", *((int*)(list->head->value)));
-	printf("tail %d\n", *((int*)(list->tail->value)));
-	printf("popBack %d\n", *((int*)popBack(list)));
-	printf("popBack %d\n", *((int*)popBack(list)));
-	printf("length %d\n", list->size);
-	printDblLinkedList(list, printInt);
-	insert(list, 1, &g);
-	printDblLinkedList(list, printInt);
-	deleteNth(list, 0);
-	printDblLinkedList(list, printInt);
-	*/
-	deleteDblLinkedList(&list);
 
+void main(){
+	DblLinkedList *list = createLinkedList();// создание списка
+	//push(list, Order); - добавление
+	//deleteNth(list, n) - удаление;
+	//deleteLinkedList(&list); - удаление всего списка
 	system("pause");
 }
 
